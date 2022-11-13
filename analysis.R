a841a8f8f57a74fdef08368aba2bd9568fccc3b4
jsta <stachel2@msu.edu>
2022-11-13 08:42:10 -0700

    cache description files

--- analysis.R
+++ analysis.R
@@ -6,20 +6,36 @@ library(dplyr)
 library(desc)
 library(gh)
 
-papers <- readRDS(gzcon(url(
-  "https://github.com/openjournals/joss-analytics/blob/gh-pages/joss_submission_analytics.rds?raw=true")))
-# head(dplyr::select(papers, starts_with("repo")))
+joss_rds <- "data/joss_R.rds"
+if (!file.exists(joss_rds)) {
+  papers <- readRDS(gzcon(url(
+    "https://github.com/openjournals/joss-analytics/blob/gh-pages/joss_submission_analytics.rds?raw=true")))
+  # head(dplyr::select(papers, starts_with("repo")))
+  dt <- dplyr::filter(papers, repo_language == "R")
+  saveRDS(dt, "data/joss_R.rds")
+}
 
-dt <- dplyr::filter(papers, repo_language == "R")
+get_desc <- function(i, dt, path = "DESCRIPTION") {
+    print(i)
+    out_path <- paste0("data/", "DESCRIPTION", "_", as.character(i))
+    if(!file.exists(out_path)){
+      download.file(paste0(dt$repo_url[i], "/raw/master/DESCRIPTION"), path)
+      Sys.sleep(runif(1, max = 3))
+      file.copy(path, out_path)
+    }
+}
 
-get_deps <- function(i){
-    download.file(paste0(dt$repo_url[i], "/raw/master/DESCRIPTION"), "DESCRIPTION")
-    d <- desc::desc()
+get_deps <- function(i) {
+    path <- paste0("data/", "DESCRIPTION", "_", as.character(i))
+    d <- desc::desc(file = path)
     deps <- d$get_deps()
     deps$pkg_name <- strsplit(dt$api_title[i], "\\:")[[1]][1]
+    deps <- dplyr::filter(deps, !grepl("R", package))
     deps
 }
 
+dt <- readRDS(joss_rds)
+lapply(seq_len(length(dt)), function(i) get_desc(i, dt))
 get_deps(2)
 
 
