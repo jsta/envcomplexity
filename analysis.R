ee97f91ecef05e5c709a2ee4fc5954ea2d228bb3
jsta <stachel2@msu.edu>
2022-11-13 09:02:37 -0700

    count deps

--- analysis.R
+++ analysis.R
@@ -7,11 +7,14 @@ library(desc)
 library(gh)
 
 joss_rds <- "data/joss_R.rds"
+# unlink(joss_rds)
 if (!file.exists(joss_rds)) {
   papers <- readRDS(gzcon(url(
     "https://github.com/openjournals/joss-analytics/blob/gh-pages/joss_submission_analytics.rds?raw=true")))
   # head(dplyr::select(papers, starts_with("repo")))
   dt <- dplyr::filter(papers, repo_language == "R")
+  dt <- dplyr::filter(papers,
+    repo_url != "https://github.com/hansenjohnson/WhaleMap")
   saveRDS(dt, "data/joss_R.rds")
 }
 
@@ -20,17 +23,27 @@ get_desc <- function(i, dt, path = "DESCRIPTION") {
     out_path <- paste0("data/", "DESCRIPTION", "_", as.character(i))
     if(!file.exists(out_path)) {
       tryCatch({
-        download.file(paste0(dt$repo_url[i], "/raw/master/DESCRIPTION"), path)
+        tryCatch({
+          download.file(paste0(dt$repo_url[i], "/raw/master/DESCRIPTION"), path)
+          Sys.sleep(runif(1, max = 3))
+          file.copy(path, out_path)
+          }, error = function(e) {
+              download.file(paste0(dt$repo_url[i], "/raw/main/DESCRIPTION"), path)
+              Sys.sleep(runif(1, max = 3))
+              file.copy(path, out_path)
+          })
         }, error = function(e) {
-            download.file(paste0(dt$repo_url[i], "/raw/main/DESCRIPTION"), path)
-        })
-      Sys.sleep(runif(1, max = 3))
-      file.copy(path, out_path)
+          print(e)
+      })
     }
 }
 
 get_deps <- function(i) {
+    print(i)
     path <- paste0("data/", "DESCRIPTION", "_", as.character(i))
+    if(!file.exists(path)){
+      return(NA)
+    }
     d <- desc::desc(file = path)
     deps <- d$get_deps()
     deps$pkg_name <- strsplit(dt$api_title[i], "\\:")[[1]][1]
@@ -40,8 +53,9 @@ get_deps <- function(i) {
 
 dt <- readRDS(joss_rds)
 lapply(seq_len(length(dt)), function(i) get_desc(i, dt))
-get_deps(2)
+deps <- lapply(seq_len(length(dt)), function(i) get_deps(i))
 
+unlist(lapply(deps, function(x) nrow(x)))
 
 # --- trying pure Zenodo
 # library(zen4R)
